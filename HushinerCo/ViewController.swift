79c84cf298955c2b319861152e90a53add32890c
danilobgervasoni <danilobgervasoni@gmail.com>
2024-06-21 16:20:47 -0300

    Initial Commit

---
+++ HushinerCo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HushinerCo
+//
+//  Created by Danilo Gervasoni on 21/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
